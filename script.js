648ca7a365bda8b670123545ebe86606a43175ed
Mulu <brianmulu007@gmail.com>
2022-08-10 19:08:20 +0300

    Finished

---
+++ script.js
@@ -0,0 +1,14 @@
+const h2 = document.createElement("h2");
+h2.textContent = "";
+document.querySelector("body").appendChild(h2);
+
+alert(`FIRST WEBSITE`)
+
+var P1 = document.getElementById("P1");
+var Div1 = document.getElementById("Div1");
+var P2 = document.getElementById("P2");
+var Div2 = document.getElementById("Div2");
+
+function changeText() {
+    P2.textContent = "DJ"
+}
